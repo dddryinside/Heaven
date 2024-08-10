8efef5d086c4d09474077656f395bcfa97a5df58
Ilya Chernikov <dddryinside@gmail.com>
2024-08-10 23:34:38 +0300

    Image page now exist, also a lot of visual functional improvements

--- src/main/java/com/dddryinside/controller/html/ImageController.java
+++ src/main/java/com/dddryinside/controller/html/ImageController.java
@@ -1,17 +1,31 @@
 package com.dddryinside.controller.html;
 
+import com.dddryinside.model.Image;
+import com.dddryinside.repository.ImageRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.Optional;
 
 @Controller
 public class ImageController {
+    private final ImageRepository imageRepository;
+
+    public ImageController(ImageRepository imageRepository) {
+        this.imageRepository = imageRepository;
+    }
+
     @GetMapping("/upload")
     public String upload() {
         return "upload-image";
     }
 
-    @GetMapping("/uploading")
-    public String uploading() {
-        return "uploading";
+    @GetMapping("/image/{id}")
+    public String image(@PathVariable Integer id, Model model) {
+        Optional<Image> imageOptional = imageRepository.findById(Long.valueOf(id));
+        model.addAttribute("image", imageOptional.orElse(null));
+        return "image-page";
     }
 }
